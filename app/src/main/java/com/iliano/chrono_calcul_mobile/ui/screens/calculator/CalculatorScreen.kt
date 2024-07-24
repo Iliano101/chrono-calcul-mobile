55c4c7c51b915bb29361b15d360b925714719596
iliano101 <iliandelagrange@gmail.com>
2024-07-24 18:49:52 -0400

    Basic UI test

--- app/src/main/java/com/iliano/chrono_calcul_mobile/ui/screens/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/iliano/chrono_calcul_mobile/ui/screens/calculator/CalculatorScreen.kt
@@ -1,8 +1,36 @@
 package com.iliano.chrono_calcul_mobile.ui.screens.calculator
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorScreen(calculatorViewModel: CalculatorViewModel = CalculatorViewModel()) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.secondaryContainer)
 
+        ) {
+            Text(text = "Heure cible")
+        }
+    }
 }
+
